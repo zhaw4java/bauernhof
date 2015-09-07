727acb1c6d56c27ec6c1a11b09fdeb6104c33212
Roger Portner <Roger Portner@EliteBook1040.home>
2015-09-07 22:23:58 +0200

    commit Bauernhof

--- src/ch/zhaw/aufgabe/bauernhof/Bauernhof.java
+++ src/ch/zhaw/aufgabe/bauernhof/Bauernhof.java
@@ -1,20 +1,28 @@
 package ch.zhaw.aufgabe.bauernhof;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Bauernhof {
 	
-	HashSet <Tier>stall = new HashSet<Tier>();
-	
-	
+//	HashSet <Tier>stall = new HashSet<Tier>();
+	private Set<Tier> stall = new HashSet<Tier>();
+//	private List<Kuh> kuhstall = new ArrayList<Kuh>();
+	  
 	public static void main(String[] args) {
 		
 	}
 	
-	public void addTier(){
-		stall.add(null);
+	public void addTier(Tier tier){
+		stall.add(tier);
 	}
 	
+	
+	public void gibLaut(){
+		for (Tier tier : stall) {
+		      tier.gibLaut();
+		    }
+	}
 	
 	
 
