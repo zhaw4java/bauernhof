756e7b07354cd7110216c589ae36bead807fc903
Roger Portner <Roger Portner@EliteBook1040.home>
2015-09-07 22:55:48 +0200

    Neue Getter/Setter-Methoden

--- src/ch/zhaw/aufgabe/bauernhof/Bauernhof.java
+++ src/ch/zhaw/aufgabe/bauernhof/Bauernhof.java
@@ -1,29 +1,54 @@
 package ch.zhaw.aufgabe.bauernhof;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public abstract class Bauernhof {
-	
-//	HashSet <Tier>stall = new HashSet<Tier>();
+
+	//	HashSet <Tier>stall = new HashSet<Tier>();
 	private Set<Tier> stall = new HashSet<Tier>();
-//	private List<Kuh> kuhstall = new ArrayList<Kuh>();
-	  
+	//	private List<Kuh> kuhstall = new ArrayList<Kuh>();
+
 	public static void main(String[] args) {
-		
+
 	}
-	
+
+
+	/*
+	 * 
+	 * Der Bauernhof hat eine Methode addTier. Damit wird dem Set ein Tier-Objekt hinzugefügt.
+	 */
 	public void addTier(Tier tier){
 		stall.add(tier);
 	}
-	
-	
+
+
+		/*
+		 * Der Bauernhof hat eine Methode lautgeben. In dieser Methode wird die gibLaut-Methode jedes Tieres, welches sich im Stall
+befindet, aufgerufen (Loop über das Set).
+		 */
 	public void gibLaut(){
 		for (Tier tier : stall) {
-		      tier.gibLaut();
-		    }
+			tier.gibLaut();
+		}
 	}
 	
 	
+	public void testgibLaut(){	
+	// Iterator wird angefordert
+	Iterator it = stall.iterator();
+	// HashSet wird mit dem Iterator durchlaufen
+	while (it.hasNext())
+	{
+	        // Next gibt das aktuelle HashSet-Objekt zurück 
+	        // und geht zum nächsten über
+	        String setText = (String) it.next();
+	        // Ausgabe des jeweiligen HashSet-Elementes
+	        System.out.println(setText);
+	}
+	}
+
+
 
 }
--- src/ch/zhaw/aufgabe/bauernhof/Kuh.java
+++ src/ch/zhaw/aufgabe/bauernhof/Kuh.java
@@ -1,10 +1,37 @@
 package ch.zhaw.aufgabe.bauernhof;
 
+	
 public class Kuh implements Tier {
 
+	private int gewicht;
+	private String name;
+	
+	public Kuh(int gewicht, String name) {
+		this.gewicht = gewicht;
+		this.name =  name;
+	}
+	
 	@Override
 	public void gibLaut() {
 		System.out.println("muuhh");
 	}
+
+	public int getGewicht() {
+		return gewicht;
+	}
+
+	public void setGewicht(int gewicht) {
+		this.gewicht = gewicht;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
