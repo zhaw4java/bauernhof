c7feaf5fda4f5a6c4de279bb1dac29c85bf962b6
Roger Portner <Roger Portner@EliteBook1040.home>
2015-09-09 19:23:54 +0200

    new BauernhofTest

---
+++ test/ch/zhaw/projekt/bauernhof/BauernhofTest.java
@@ -0,0 +1,14 @@
+package ch.zhaw.projekt.bauernhof;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BauernhofTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
